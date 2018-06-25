f4712df2ce688754b15e3ef5232e5a8c6c17cb42
Bin <darren9352@gmail.com>
2018-06-25 22:40:53 +0900

    commit

--- main/views.py
+++ main/views.py
@@ -5,23 +5,37 @@ from .models import Product, Category
 
 def index(request):
     products = Product.objects.all()
-    context = {'products':products}
+    categorys = Category.objects.all()
+    context = {'products':products, 'categorys': categorys}
     return render(request, 'main/index.html', context)
 
 def checkout(request):
-    return render(request, 'main/checkout.html')
+    categorys = Category.objects.all()
+    context = {'categorys': categorys}
+    return render(request, 'main/checkout.html', context)
 
 def contact(request):
-    return render(request, 'main/contact.html')
+    categorys = Category.objects.all()
+    context = {'categorys': categorys}
+    return render(request, 'main/contact.html', context)
 
-def product_detail(request):
-    return render(request, 'main/product_detail.html')
+def product_details(request, productcode):
+    products = Product.objects.filter(productcode = productcode)
+    categorys = Category.objects.all()
+    context = {'products':products, 'productcode':productcode,'categorys': categorys}
+    return render(request, 'main/product_detail.html', context)
 
 def products(request):
-    return render(request, 'main/products.html')
+    categorys = Category.objects.all()
+    context = {'categorys': categorys}
+    return render(request, 'main/products.html', context)
 
 def cart(request):
-    return render(request, 'main/cart.html')
+    categorys = Category.objects.all()
+    context = {'categorys': categorys}
+    return render(request, 'main/cart.html', context)
 
 def register(request):
-    return render(request, 'main/register.html')
+    categorys = Category.objects.all()
+    context = {'categorys': categorys}
+    return render(request, 'main/register.html', context)
